ae21bdfcc2a4dab6d45a0c1cb347aa5a69e58d15
Max Luebbers <luebbersma@gmail.com>
2021-08-29 20:16:57 -0500

    Adds Netlify CMS functionality

--- scripts/data/getScheduleData.js
+++ scripts/data/getScheduleData.js
@@ -5,5 +5,5 @@ export default function getScheduleData() {
     const fileName = path.join(path.join(process.cwd(), 'public/content', 'schedule.json'));
     const fileContents = fs.readFileSync(fileName, 'utf8');
     const jsonResult = JSON.parse(fileContents);
-    return jsonResult;
+    return jsonResult.dates;
 }
